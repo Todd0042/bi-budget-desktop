21e3b78d4cac4be7dc2ec4ebdceeb2c72918bf1c
Todd0042 <scottrb1982@gmail.com>
2026-04-17 04:41:42 -0400

    nix flake file

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Bi-Budget Desktop (Python + PySide6)";
+  description = "BiBudget packaged as a Nix app";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,17 +10,39 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.python313
-            pkgs.python313Packages.pyside6
-            pkgs.qt6.full
-          ];
 
-          # Makes Qt apps (like PySide6) find their plugins
-          QT_PLUGIN_PATH = "${pkgs.qt6.full}/lib/qt-6/plugins";
-          QML2_IMPORT_PATH = "${pkgs.qt6.full}/lib/qt-6/qml";
+        python = pkgs.python312;
+        pythonEnv = python.withPackages (ps: [
+          ps.pyside6
+        ]);
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "bibudget";
+          version = "1.0";
+
+          src = ./.;
+
+          buildInputs = [ pythonEnv ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            mkdir -p $out/app
+
+            # Copy your source code
+            cp -r $src/* $out/app/
+
+            # Create launcher script
+            cat > $out/bin/bibudget <<EOF
+#!${pythonEnv}/bin/python
+import sys
+sys.path.insert(0, "$out/app")
+import main
+EOF
+
+            chmod +x $out/bin/bibudget
+          '';
         };
-      });
+      }
+    );
 }
